a6e64448070363558ecec56565a29494c3adf381
pauloremoli <paulo002@gmail.com>
2022-03-13 01:21:00 +0100

    change rotation, scale and position

--- src/script.js
+++ src/script.js
@@ -3,10 +3,39 @@ import * as THREE from "three";
 const canvas = document.querySelector(".webgl");
 
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: 0xffff00 })
+);
+group.add(cube1);
+
+cube1.position.x = 0.7;
+cube1.position.y = -0.6;
+cube1.position.z = -3;
+
+// force the order to apply the rotation
+cube1.rotation.reorder("YXZ");
+
+cube1.rotation.x = 0.2;
+cube1.rotation.y = 1;
+cube1.rotation.z = -1;
+
+const cube2 = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({ color: "red" })
+);
+
+cube2.position.z = -3;
+cube2.position.x = -1;
+
+group.add(cube2);
+
+const axisHelper = new THREE.AxisHelper();
+scene.add(axisHelper);
 
 const sizes = {
 	width: window.innerWidth,
@@ -15,6 +44,10 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
 camera.position.z = 3;
+camera.position.y = 0.3;
+camera.position.x = 0.3;
+
+camera.lookAt(group.position);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
